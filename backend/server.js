224e73a61da6f25c508a7ced8af6e478be1183fd
Abhilash <abhilash.vc888@gmail.com>
2025-06-16 21:18:47 +0530

    added

--- backend/server.js
+++ backend/server.js
@@ -10,19 +10,12 @@ connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
 app.use("/api", authRoutes);
-
-app.use(express.static(path.join(__dirname, "/frontend/build")));
-
-// Serve React frontend for any unknown route
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  console.log(__dirname);
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
